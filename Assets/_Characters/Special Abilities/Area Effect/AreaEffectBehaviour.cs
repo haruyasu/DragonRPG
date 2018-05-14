4bb6bd915823afcd49dc382bc65a5b08efa76f60
haruyasu <harukun2002@gmail.com>
2018-05-13 20:51:47 -0700

    3-115 Create An Area Of Effect Ability

---
+++ Assets/_Characters/Special Abilities/Area Effect/AreaEffectBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+using RPG.Core;
+
+public class AreaEffectBehaviour : MonoBehaviour, ISpecialAbility {
+
+    AreaEffectConfig config;
+
+    public void SetConfig(AreaEffectConfig configToSet) {
+        this.config = configToSet;
+    }
+
+    // Use this for initialization
+    void Start() {
+        print("Area Effect behaviour attached to " + gameObject.name);
+    }
+
+    public void Use(AbilityUseParams useParams) {
+        print("Area Effect used by " + gameObject.name);
+
+        // Statoc sphere cast for targets
+        RaycastHit[] hits = Physics.SphereCastAll(
+            transform.position,
+            config.GetRadius(),
+            Vector3.up,
+            config.GetRadius()
+        );
+
+        foreach (RaycastHit hit in hits) {
+            var damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+
+            if (damageable != null) {
+                float damageToDeal = useParams.baseDamage + config.GetDamageToEachTarget();
+                damageable.TakeDamage(damageToDeal);
+            }
+        }
+    }
+}
